4efef12b55a77dcffd9d05e220098778edcf4eca
Vaibhav Bansal <103635777+SoloWarrior01@users.noreply.github.com>
2023-08-08 11:42:50 +0530

    Update integrate_bluetooth.py

--- integrate_bluetooth.py
+++ integrate_bluetooth.py
@@ -1,13 +1,15 @@
 import serial
-uart_channel = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2)
-
-data = ""
-data1 = ""
-while True:
-    data = uart_channel.read(1)
-    data1 += data
-    print(data1)
+def bluetooth_call():
+    uart_channel = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2)
     
-    uart_channel.flush()
     data = ""
     data1 = ""
+    while True:
+        data = uart_channel.read(1)
+        data1 += data
+        print(data1)
+        
+        uart_channel.flush()
+        data = ""
+        data1 = ""
+    
